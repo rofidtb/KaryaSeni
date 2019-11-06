28014add1d2c1a2b16b6eb993d3788e7abd4e00e
rofidtb <43767826+rofidtb@users.noreply.github.com>
2019-11-06 16:02:05 +0700

    Initial commit

---
+++ app/src/main/java/android/jplas/karyaseni/SplashActivity.java
@@ -0,0 +1,26 @@
+package android.jplas.karyaseni;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
